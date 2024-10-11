7140b6a359e8f5e009ba0e9e9e506c7ded699239
reechelly <elijah.reech@strathmore.edu>
2024-10-11 10:20:12 +0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <algorithm>
+#include <random>
+#include <iostream>
+#include <unordered_set>
+#include <vector>
+#include <sstream>
+#include <fstream>
+#include <iomanip> // for setting time format
+
+struct Team {
+    std::string name;
+    std::string stadium;
+    std::string town;
+};
+
+
